4968415c86006c1697c0d45ed7eeccf4d354a9d3
privetitsmee <110345831+privetitsmee@users.noreply.github.com>
2023-09-20 16:02:49 +0300

    add netlify functions

--- src/Components/Footer/Form.jsx
+++ src/Components/Footer/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
     
         try {
           // Отправляем данные на сервер
-          const response = await axios.post('http://localhost:3001/submit', formData);
+          const response = await axios.post('/.netlify/functions/form', formData);
     
           if (response.status === 200) {
             // Успешное сообщение о отправке
@@ -45,7 +45,7 @@ const Form = () => {
       };
 
   return (
-    <form action="/.netlify/functions/form" className={s.form} id='form'>
+    <form className={s.form} id='form'>
                 <div className={s.body}>
                     <input name='name' onChange={handleChange} value={formData.name} className={s.input} type="text" placeholder='Ваше имя...'/>
                     <input name='number' onChange={handleChange} value={formData.number} className={s.input} type="number" placeholder='Ваш номер...'/>
