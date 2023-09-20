4aa8906af82f3d9fe35c0acd9105e2a4ab57fb97
privetitsmee <110345831+privetitsmee@users.noreply.github.com>
2023-09-20 19:35:13 +0300

    add netlify functions

--- netlify/functions/form.js
+++ netlify/functions/form.js
@@ -6,16 +6,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-// const corsOptions = {
-//   origin: 'https://main--stalwart-phoenix-a575dc.netlify.app/', // Замените на URL вашего сайта на Netlify
-//   methods: 'POST', // Укажите методы, которые вы разрешаете
-// };
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 const telegramToken = '6605316205:AAGtyrTHiUEnY0ipJ2rqrxQSp_aHVSSDfOg';
 const chatId = '-1001833739048'; // Используйте Chat ID из предыдущего шага
 
-app.post('/submit', (req, res) => {
+app.post('/.netlify/functions/form', (req, res) => {
   const { name, number, message } = req.body;
 
   const telegramMessage = `
