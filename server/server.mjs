5e51043b53c20827888128fb2887ae4aa54d19ed
privetitsmee <110345831+privetitsmee@users.noreply.github.com>
2023-09-20 19:14:45 +0300

    add netlify functions

--- server/server.mjs
+++ server/server.mjs
@@ -6,10 +6,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-const corsOptions = {
-  origin: 'https://main--stalwart-phoenix-a575dc.netlify.app/', // Замените на URL вашего сайта на Netlify
-  methods: 'POST', // Укажите методы, которые вы разрешаете
-};
+// const corsOptions = {
+//   origin: 'https://main--stalwart-phoenix-a575dc.netlify.app/', // Замените на URL вашего сайта на Netlify
+//   methods: 'POST', // Укажите методы, которые вы разрешаете
+// };
 app.use(cors(corsOptions));
 
 const telegramToken = '6605316205:AAGtyrTHiUEnY0ipJ2rqrxQSp_aHVSSDfOg';
